a899d93ae5bf3fa9bd4c46de3a67200a46ce6364
lahirutech <rmllbrathnayaka@gmail.com>
2020-11-07 19:55:26 +0530

    Adding the basic validation using express validator

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -2,7 +2,10 @@ const express = require("express");
 const router = express.Router();
 const { signup } = require("../controllers/auth");
 
+const {runValidation}=require("../validators/")
+const {userSignupValidator}=require("../validators/auth")
+
 // routes
-router.post("/signup", signup);
+router.post("/signup", userSignupValidator,runValidation,signup);
 
 module.exports = router;
