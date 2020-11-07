ff11c60510cb1fcee2ecb841d4709858f5c75138
lahirutech <rmllbrathnayaka@gmail.com>
2020-11-07 21:21:27 +0530

    adding the sign in route

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -3,9 +3,10 @@ const router = express.Router();
 const { signup } = require("../controllers/auth");
 
 const {runValidation}=require("../validators/")
-const {userSignupValidator}=require("../validators/auth")
+const {userSigninValidator,userSignupValidator}=require("../validators/auth")
 
 // routes
 router.post("/signup", userSignupValidator,runValidation,signup);
+router.post("/signin", userSigninValidator,runValidation,signin);
 
 module.exports = router;
