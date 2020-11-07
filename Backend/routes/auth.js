a7fc4c4c726a469639e607ab4df360aa8a571f58
lahirutech <rmllbrathnayaka@gmail.com>
2020-11-08 00:45:31 +0530

    Adding signint signout fnctions

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -1,12 +1,25 @@
 const express = require("express");
 const router = express.Router();
-const { signup } = require("../controllers/auth");
+const {
+  signup,
+  signin,
+  signout,
+  requireSignin,
+} = require("../controllers/auth");
 
-const {runValidation}=require("../validators/")
-const {userSigninValidator,userSignupValidator}=require("../validators/auth")
+const { runValidation } = require("../validators/");
+const {
+  userSigninValidator,
+  userSignupValidator,
+} = require("../validators/auth");
 
 // routes
-router.post("/signup", userSignupValidator,runValidation,signup);
-router.post("/signin", userSigninValidator,runValidation,signin);
+router.post("/signup", userSignupValidator, runValidation, signup);
+router.post("/signin", userSigninValidator, runValidation, signin);
+router.get("/signout", signout);
+
+router.get("/secret", requireSignin, (req, res) => {
+  return res.json({ msg: "secret" });
+});
 
 module.exports = router;
