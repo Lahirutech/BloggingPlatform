6b22eabc5bb0ee97aff97400651dddb51105e59b
lahirutech <rmllbrathnayaka@gmail.com>
2020-11-28 21:47:54 +0530

    Added new routes

--- Backend/server.js
+++ Backend/server.js
@@ -9,30 +9,34 @@ require("dotenv").config();
 // Routes
 const blogRoutes = require("./routes/blog");
 const authRoutes = require("./routes/auth");
+const userRoutes = require("./routes/user");
+const categoryRoutes = require("./routes/category");
+const tagRoutes = require("./routes/tags");
 
 const app = express();
-// configure the app
-
-// middlewares
-app.use(morgan("dev"));
-app.use(bodyParser.json());
-app.use(cookieParser());
 
 mongoose
   .connect(process.env.DATABASE, {
     useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
     useUnifiedTopology: true,
   })
   .then(() => console.log("db Connected"));
 
-// cors
-if (process.env.NODE_ENV == "development") {
-  app.use(cors({ origin: `${process.env.CLIENT_URL}` }));
-}
+// middlewares
+app.use(cors());
+
+app.use(morgan("dev"));
+app.use(bodyParser.json());
+app.use(cookieParser());
 
 // routes middleware
-app.use("/api", blogRoutes);
 app.use("/api", authRoutes);
+app.use("/api", blogRoutes);
+app.use("/api", userRoutes);
+app.use("/api", categoryRoutes);
+app.use("/api", tagRoutes);
 
 // port
 const port = process.env.PORT || 8000;
